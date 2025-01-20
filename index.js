0dad487a0859057cf69c375acb32296b2e988f43
connor-r78 <155401101+connor-r78@users.noreply.github.com>
2025-01-20 17:19:47 -0500

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("🚀 Hello from Render!");
-});
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
