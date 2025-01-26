a8e13900cab3a9bce821f2a67524bb698c4b9e40
connor-r78 <155401101+connor-r78@users.noreply.github.com>
2025-01-26 17:37:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ serv.listen(2000);
 var io = require("socket.io") (serv,{});
 io.sockets.on("connection", function(socket) {
   console.log("socket connection");
-  socket.on("happy",function(data) {
-    console.log("happy because " + data.reason);
-  });
-
-  socket.emit("serverMsg", {
-    msg:"hello",
+  socket.on("someone buzzed",function(data) {
+    console.log("buzz from " + data.playerID);
   });
 });
